d313500f2cd3110cd237a516719fb9a6e7fadc7f
Themp10 <oussamaaboujaafar@gmail.com>
2024-04-23 08:21:47 +0000

    f

--- db/db_conn.php
+++ db/db_conn.php
@@ -10,9 +10,9 @@ if(session_status()==PHP_SESSION_NONE){
 // } 
     // connexion à la base de données
     $db_username = 'sa';
-    $db_password = 'Thethepo06+';
+    $db_password = 'MG+P@ssw0rd';
     $db_name     = 'pf_test';
-    $db_host     = '127.0.0.1';
+    $db_host     = '172.28.0.22';
     $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
            or die('could not connect to database');
 
--- db/db_send.php
+++ db/db_send.php
@@ -1,10 +1,10 @@
 <?php
 require_once('../util/functions.php');
 check_connected();
-  $db_username = 'root';
-  $db_password = '';
+  $db_username = 'sa';
+  $db_password = 'MG+P@ssw0rd';
   $db_name     = 'pf_test';
-  $db_host     = 'localhost';
+  $db_host     = '172.28.0.22';
   $conn = mysqli_connect($db_host, $db_username, $db_password,$db_name)or die('could not connect to database');
   
 
